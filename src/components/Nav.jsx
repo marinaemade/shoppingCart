afdb728c4f6f94d2b072adf01f06c33164ed26cb
marinaemade <marmouraemade.com>
2026-04-02 20:36:56 +0200

    edit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,9 +9,7 @@ const Nav = () => {
             viewBox="0 0 24 24"
             className="inline-block h-5 w-5 stroke-current">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
               d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
